0565fe361a92857827f504481c0d1b8777920d27
Jessica Adzoyi <121783593+JESSICA047@users.noreply.github.com>
2024-10-21 14:46:10 +0000

    Mobile media queries only

---
+++ main.js
@@ -0,0 +1,11 @@
+function myFunction(x) {
+  x.classList.toggle("change");
+  document.querySelector('.menu').classList.toggle('show');
+}
+
+// script.js
+
+// document.getElementById('container').addEventListener('click', function() {
+//     var navbar = document.getElementById('navbar');
+//     navbar.classList.toggle('active');
+// });
